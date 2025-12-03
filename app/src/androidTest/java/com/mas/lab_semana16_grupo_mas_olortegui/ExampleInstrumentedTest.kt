44b0f48fecdf49be3aa0f09abb541b1568069b0c
Strike32GT <roberto.mas@tecsup.edu.pe>
2025-12-03 13:23:35 -0500

    primer commit

---
+++ app/src/androidTest/java/com/mas/lab_semana16_grupo_mas_olortegui/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.mas.lab_semana16_grupo_mas_olortegui
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.mas.lab_semana16_grupo_mas_olortegui", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/mas/lab_semana16_grupo_mas_olortegui/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mas.lab_semana16_grupo_mas_olortegui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mas.lab_semana16_grupo_mas_olortegui.ui.theme.Lab_Semana16_Grupo_Mas_OlorteguiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab_Semana16_Grupo_Mas_OlorteguiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab_Semana16_Grupo_Mas_OlorteguiTheme {
+        Greeting("Android")
+    }
+}
